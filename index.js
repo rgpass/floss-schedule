49c297ad552568c22a5180d53524a72b3d22cd77
Gerry Pass <rgpass@gmail.com>
2018-09-09 19:56:25 -0400

    :tada: Hello World

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+require('dotenv').config()
+
+const app = express()
+const { PORT } = process.env
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => res.render('index'))
+
+app.listen(PORT, () => console.log(`⚡️⚡️ Server started on port ${PORT}`))
